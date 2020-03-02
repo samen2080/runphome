f601524660c9292674cb0b989d312fbf892475f5
samen2080@126.com <samen2080@126.com>
2020-03-02 17:14:12 +0800

    0.0.7 sample display

--- pages/index/product/product.js
+++ pages/index/product/product.js
@@ -2,11 +2,13 @@
 const app = getApp()
 var imgPostf = ["jpg", "png", "bmp", 'jpeg'];
 var videoPostf = ['avi', 'rm', 'rmvb', 'mpeg', 'mpg', 'dat', 'mov', 'qt', 'asf', 'wmv', 'mp4'];
+var routes = require('routes'); 
 Page({
 
   /**
    * 页面的初始数据
    */
+
   data: {
     ellipsis: true,
     currentTab: 0,
@@ -56,8 +58,14 @@ Page({
         url: '',
         icon: ''
       }
-    ]  
+    ],
+
+
+    userInfo: {},
+    cellHeight: '120px',
+    pageItems: [] ,
   },
+
   ellipsis: function () {
     var value = !this.data.ellipsis;
     this.setData({
@@ -73,18 +81,18 @@ Page({
     var user_id = wx.getStorageSync("user_info").user_id;
     that.setData({
       host: host,
-      com_id: options.com_id,
+      // com_id: options.com_id,
       user_id: user_id
-    })
+    });
+
     wx.getStorage({
       key: 'openid',
       success: function (res) {
         that.setData({
           openid: res.data
         })
-        that.getDetail();
       },
-    })
+    });
 
     app.func.req('get_product', { user_id: that.data.user_id }, 'GET', function (res) {
       console.log("20200288 res", res);
@@ -93,106 +101,67 @@ Page({
       })
     });
 
-
-
-
-
-
-
-
-  },
-
-  // 20200213 start
-
-
-  getDetail: function () {
-    var that = this;
-    // app.func.req('my_company_show', {id: that.data.com_id }, 'GET', function (res) {
-    app.func.req('my_company_show/' + that.data.com_id, {}, 'GET', function (res) {
-      console.log("20200213res", res);
+    app.func.req('get_sample', { user_id: that.data.user_id, pageSize: 9, page: 1 }, 'GET', function (res) {
+      console.log("20200288 sample res", res);
       that.setData({
-        items: res,
-        com_img: res.com_img
+        sampleList: res
+      });
+      // for (var i = 0; i < that.data.sampleList.length; i++) {
+      //   json.push({ routers.i['url']: rows[i]['ORDERFORM_NO'], SALE_NO: rows[i]['SALE_NO']});
+      // };
+      wx.setNavigationBarTitle({
+        title: '案例展示-' + '九宫格',
+        success: function (res) {
+          // success 
+        }
       })
-    });
-  },
+
+      var pageItems = [];
+      for (var i = 0; i < that.data.sampleList.length; i++) {
+        routes.PageItems[i].icon = that.data.host + that.data.sampleList[i].smd_img[0];
+        routes.PageItems[i].text = "案例" + that.data.sampleList[i].smd_id;
+        routes.PageItems[i].route = "sample/sample?smd_id=" + that.data.sampleList[i].smd_id;
+        console.log("20200302 pageitems:", routes.PageItems[i].icon);
+      };
 
 
-  getIn: function () {
-    var that = this;
-    app.func.req('my_company_job/' + that.data.com_id, {}, 'GET', function (res) {
-      console.log("20200213res", res);
-      that.setData({
-        inList: res
-      })
-    });
-  },
-  // 20200213 end
 
-  //点击切换
-  clickTab: function (e) {
-    var that = this;
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: e.target.dataset.current
-      })
-      if (e.target.dataset.current == 0) {
-        that.getDetail();
-      } else {
-        that.getIn();
-      }
-    }
-  },
-  masterPost: function (e) {
-    var that = this;
-    that.setData({
-      currentTab: 1
-    })
-  },
-  // 单个操作
-  listenerButton: function (e) {
-    var that = this;
-    var arr = that.data.inList;
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i].in_id == e.currentTarget.dataset.inid) {
-        if (arr[i].collect == 0) {
-          that.inCollect(["收藏"], e.currentTarget.dataset.inid);
-        } else if (arr[i].collect == 1) {
-          that.inCollect(["取消收藏"], e.currentTarget.dataset.inid);
+      var row = [];
+      var len = routes.PageItems.length;//重组PageItems 
+      // var len = that.data.sampleList.length;//重组PageItems 
+      len = Math.floor((len + 2) / 3) * 3;
+      for (var i = 0; i < len; i++) {
+        if ((i + 1) % 3 == 0) {
+          row.push(routes.PageItems[i]);
+          // row.push(that.data.sampleList[i]);
+          pageItems.push(row);
+          row = [];
+          continue;
+        }
+        else {
+          row.push(routes.PageItems[i]);
+          // row.push(that.data.sampleList[i]);
         }
       }
-    }
-  },
-  // 内容精选收藏
-  inCollect: function (item, coll_good_id) {
-    var that = this;
-    wx.showActionSheet({
-      itemList: item,//显示的列表项
-      itemColor: '#000000',
-      success: function (res) {
-        if (res.tapIndex === 0) {
-          app.func.req('collect', { coll_type: 1, coll_user_id: that.data.openid, coll_good_id: coll_good_id }, 'POST', function (res) {
-            // console.log(res);
-            if (res.code == 200) {
-              that.getIn();
-            }
-          });
+      wx.getSystemInfo({
+        success: function (res) {
+          var windowWidth = res.windowWidth;
+          that.setData({
+            cellHeight: (windowWidth / 3) + 'px'
+          })
+        },
+        complete: function () {
+          that.setData({
+            pageItems: pageItems
+          })
         }
-      },
-      fail: function (res) { },
-      complete: function (res) { }
-    })
-  },
-  // 关注/取消
-  focus: function (e) {
-    var that = this;
-    app.func.req('follow_user', { openid: that.data.openid, fo_followed_id: that.data.user_id }, 'POST', function (res) {
-      // console.log(res);
-      that.getDetail();
+      })
     });
   },
+
+
+
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/index/product/sample/sample.js
+++ pages/index/product/sample/sample.js
@@ -1,18 +1,75 @@
 // pages/index/product/sample/sample.js
+const app = getApp()
+var imgPostf = ["jpg", "png", "bmp", 'jpeg'];
+var videoPostf = ['avi', 'rm', 'rmvb', 'mpeg', 'mpg', 'dat', 'mov', 'qt', 'asf', 'wmv', 'mp4'];
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    currentTab: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    var host = getApp().globalData.host;
+    that.setData({
+      host: host,
+      smd_id: options.smd_id
+    })
+    wx.getStorage({
+      key: 'openid',
+      success: function (res) {
+        that.setData({
+          openid: res.data
+        })
+        that.getDetail();
+      },
+    })
+  },
 
+
+  getDetail: function () {
+    var that = this;
+    app.func.req('get_sample_show/' + that.data.smd_id, {}, 'GET', function (res) {
+      console.log("20200213res", res);
+      that.setData({
+        items: res,
+        smd_img: res.smd_img
+      })
+    });
+  },
+
+  getIn: function () {
+    var that = this;
+    app.func.req('my_company_job/' + that.data.com_id, {}, 'GET', function (res) {
+      console.log("20200213res", res);
+      that.setData({
+        inList: res
+      })
+    });
+  },
+  // 20200213 end
+
+  //点击切换
+  clickTab: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+      if (e.target.dataset.current == 0) {
+        that.getDetail();
+      } else {
+        that.getIn();
+      }
+    }
   },
 
   /**
